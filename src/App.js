cee23fb94b074b8892ec70bba5f12855278cf614
Дмитрий <harchenkodmitrii@bk.ru>
2020-02-07 12:13:55 +0300

    bag fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import './index.css';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Resume from './components/Resume';
+import Contacts from './components/Contacts/container';
+import Portfolio from './components/Portfolio/container';
+import {Provider} from 'react-redux';
+import store from './redux/redux-store';
+import Project from './components/Project/container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Provider store={store}>
+        <div className={'App'}>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Profile}/>
+            <Route exact path="/resume" component={Resume}/>
+            <Route exact path="/contacts" component={Contacts}/>
+            <Route exact path="/portfolio" component={Portfolio}/>
+            <Route exact path="/portfolio/:id" component={Project}/>
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
